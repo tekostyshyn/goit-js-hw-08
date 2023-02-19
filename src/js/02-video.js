898466059f9c35ab36fa120bea36dee61457ff07
tekostyshyn <tekostyshyn@gmail.com>
2023-02-19 20:17:57 +0200

    second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const onPlay = function (data) {
+  const currentTime = JSON.stringify(data.seconds);
+  localStorage.setItem('videoplayer-current-time', currentTime);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function setCurrentTime() {
+  const currentTimeFromStorage = localStorage.getItem('videoplayer-current-time');
+  if (currentTimeFromStorage) {
+    player
+      .setCurrentTime(currentTimeFromStorage)
+      .then(function (seconds) {})
+      .catch(function (error) {
+        switch (error.name) {
+          case 'RangeError':
+            break;
+
+          default:
+            break;
+        }
+      });
+  }
+};
+
+setCurrentTime();
